0ab5286fade81ffd60375213de92972717a0ddcd
smvanliempd <smvanliempd@gmail.com>
2020-05-12 20:15:19 +0200

    minor change

--- load_functions.R
+++ load_functions.R
@@ -10,6 +10,7 @@ library(car)
 library(multcomp)
 library(digest)
 library(xlsx)
+library(cowplot)
 
 # analysis
 source(paste0(fun.loc,"read_fun.R"))
@@ -36,6 +37,7 @@ source(paste0(fun.loc,"get_stan_samples.R"))
 source(paste0(fun.loc,"id_bind_samples_fun.R"))
 
 #plotting
+source(paste0(fun.plot.loc,"plot_raw.R"))
 source(paste0(fun.plot.loc,"dot_plots_per_class.R"))
 source(paste0(fun.plot.loc,"dot_plots_per_class_ext.R"))
 source(paste0(fun.plot.loc,"plot_diff_strain_3dpi.R"))
